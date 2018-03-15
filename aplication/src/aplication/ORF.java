7950b0328899327f0ec7b1d7d960f9378fbc18a2
moniekvanselst <MAA.vanSelst@student.han.nl>
2018-03-15 14:02:31 +0100

    met classes

--- aplication/src/aplication/ORF.java
+++ aplication/src/aplication/ORF.java
@@ -9,13 +9,6 @@ package aplication;
  *
  * @author van Selst
  */
-public class Aplication {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ORF {
     
 }
