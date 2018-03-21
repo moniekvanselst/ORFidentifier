8a29698350d29dbc3c0b1d5b44837a5fbba8e460
Will000 <31846085+Willy000@users.noreply.github.com>
2018-03-21 11:04:41 +0100

    Add files via upload
    Maven project aangemaakt van het lopende project en BioJava bij dependencies toegevoegd.

---
+++ Applicatie/src/main/java/Sequentie.java
@@ -0,0 +1,42 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author van Selst
+ */
+public class Sequentie {
+    private int seqID;
+    private String seq;
+ 
+    
+          
+    
+    public Sequentie(String seq, int seqID){
+    this.seq = seq;
+    this.seqID = seqID;
+    }
+
+    public int getSeqID() {
+        return seqID;
+    }
+
+    public void setSeqID(int seqID) {
+        this.seqID = seqID;
+    }
+
+    public String getSeq() {
+        return seq;
+    }
+
+    public void setSeq(String seq) {
+        this.seq = seq;
+    }
+    
+}
